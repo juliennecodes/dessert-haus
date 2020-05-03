75508e338ec020135871e7cc8b0b4e1bddac9601
julienne <julienne.codes@gmail.com>
2020-05-02 22:47:10 -0400

    Remove comments from burger js
    -cleaned up code in the js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,31 +1,9 @@
-//Plain description
-//When you click on the burger Menu
-//the list of menu is displayed
-//when you click it again
-//the list of menu disappears
-
-// Part 1 - the menu is displayed
-//Get the relevant elements
-
-let menu = document.getElementsByClassName("jsMenu")[0];
-
-//Menu is displayed
-menu.classList.toggle("active");
-
-//showMenu function
-function showMenu(){
-  menu.classList.toggle("active");
-}
-
-//When you click the burger
-let burger = document.getElementsByClassName("jsBurger")[0];
-burger.onclick = function(){showMenu();};
-
-//Cleaned up code
+//Show menu
 function showMenu(){
   let menu = document.getElementsByClassName("jsMenu")[0];
   menu.classList.toggle("active");
 }
 
+//Event listener for showMenu
 let burger = document.getElementsByClassName("jsBurger")[0];
-burger.onclick = function(){showMenu();};
+burger.onclick = function(){ showMenu(); };
